2efda70ba7795350c68b6b243ba54d3bcb06555f
Andries N. Mogashoa <106239836+katGhost@users.noreply.github.com>
2026-01-23 10:34:24 +0000

    taxi zones: clean up -> click commands executable

--- pipeline/upload-data.py
+++ pipeline/upload-data.py
@@ -1,44 +1,38 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import pandas as pd
+from sqlalchemy import create_engine
+import click
 
+@click.command()
+@click.option('--pg-user', default='root')
+@click.option('--pg-pass', default='root')
+@click.option('--pg-host', default='localhost')
+@click.option('--pg-port', default=5432)
+@click.option('--pg-db', default='ny_taxi')
+@click.option('--target-table', default='taxi_zones')
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
 
-# In[2]:
-
-
-url = 'https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv'
-
-
-# In[3]:
-
-
-df_zones = pd.read_csv(url)
-
-df_zones.head()
-
-
-# In[ ]:
-
-
-taxi_zones
-
-for df_zone in df_zones:
-    if first:
-        # Create table schema (no data)
-        df_zone.head(0).to_sql(
-            name=,
-            con=engine,
-            if_exists="replace"
-        )
-
-    # Insert chunk
-    df_zone.to_sql(
-        name=target_table,
-        con=engine,
-        if_exists="append"
+    engine = create_engine(
+        f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
     )
 
+    url = "https://d37ci6vzurychx.cloudfront.net/misc/taxi_zone_lookup.csv"
+
+    df = pd.read_csv(url)
+
+    # Normalize column names
+    df.columns = [c.lower() for c in df.columns]
+
+    df.to_sql(
+        name=target_table,
+        con=engine,
+        if_exists='replace',
+        index=False
+    )
+
+    print(f"Loaded {len(df)} rows into {target_table}")
+
+if __name__ == '__main__':
+    run()
